b975b7ada2a902765d33bfaeefe71dc02db92478
Uonai <cs.mccaleb@gmail.com>
2020-11-07 16:26:38 -0600

    Formatting updates for blogroll, blog, logo

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -94,17 +94,6 @@ export const IndexPageTemplate = ({
                     </Link>
                   </div>
                 </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
