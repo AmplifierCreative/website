f22aaa9a26381bc954adbb55077559a956b9694c
Jordan Booker <jordan.m.booker@gmail.com>
2020-11-08 00:01:29 -0500

    Added logo and more footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,96 +10,118 @@ const footer = {
   backgroundColor: '#2D2C2C',
 };
 
+const socialCircle = {    
+  borderRadius: "50%",
+  width: "4rem",
+  height: "4rem",
+  background: "#fff",
+}
+
+const socialSvg = {
+  width: "90%",
+  height: "auto",
+  display: "block", 
+  marginRight: "auto",
+  marginLeft: "auto",
+}
+
 const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer" style={footer}>
-        <div>
           <div className="container">
             <div className="columns">
-              <div className="column is-half">
+              <div className="column is-two-thirds">
                 <input className="input is-medium" type="email" placeholder="enter your email address to stay in touch"/>
+                <div className="columns">
+                  <div className="column is-2">
+                    <section className="menu">
+                      <ul className="menu-list">
+                        <li>
+                          <Link to="/" className="navbar-item">
+                            About
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="navbar-item" to="/about">
+                            Services
+                          </Link>
+                        </li>
+                      </ul>
+                    </section>
+                  </div>
+                  <div className="column is-2">
+                    <section>
+                      <ul className="menu-list">
+                        <li>
+                          <Link className="navbar-item" to="/blog">
+                            Portfolio
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="navbar-item" to="/contact">
+                            Blog
+                          </Link>
+                        </li>
+                      </ul>
+                    </section>
+                  </div>
+                  <div className="column is-2">
+                    <section>
+                      <ul className="menu-list">
+                        <li>
+                          <Link className="navbar-item" to="/blog">
+                            Contact
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="navbar-item" to="/contact">
+                            Privacy
+                          </Link>
+                        </li>
+                      </ul>
+                    </section>
+                  </div>
+                </div>
+                <div className="columns">
+                  <h5>&#169; Amplifier Creative 2020 view our <Link href="#" className="link">Terms of Use</Link> and <Link href="#" className="link">Privacy Policy</Link></h5>
+                </div>
               </div>
-              <div className="column is-half">
-                <h5 className="column is-half is-3">Follow us</h5>
+              <div className="column is-one-third">
+                <div className="columns">
+                  <div className="column">
+                    <h5>Follow us</h5>
+                  </div>
+                </div>
+                <div className="columns">
+                  <div className="column is-2" style={socialCircle}>
+                    <a title="twitter" href="#">
+                      <img
+                        style={socialSvg}
+                        src={twitter}
+                        alt="Twitter"
+                      />
+                    </a>
+                </div>
+                <div className="column is-2" style={socialCircle}>
+                  <a title="instagram" href="#">
+                    <img
+                      style={socialSvg}
+                      src={instagram}
+                      alt="Instagram"
+                    />
+                  </a>
+                </div>
+                <div className="column is-2" style={socialCircle}>
+                  <a title="linkedin" href="#">
+                    <img
+                      style={socialSvg}
+                      src={linkedin}
+                      alt="LinkedIn"
+                    />
+                  </a>
+                </div>
               </div>
-            </div>
-            <div style={{ maxWidth: '100vw' }} className="columns">
-              <div className="column is-2">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        Services
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-2">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Portfolio
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Blog
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-2">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Contact
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Privacy
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="twitter" href="#">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="#">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="placeholder" href="#">
-                  <img
-                    src={linkedin}
-                    alt="LinkedIn"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
-            </div>
-            <div style={{ maxWidth: '100vw' }} className="columns">
-              <h5>&#169; Amplifier Creative 2020 view our <Link href="#" className="link">Terms of Use</Link> and <Link href="#" className="link">Privacy Policy</Link></h5>
             </div>
           </div>
         </div>
