2a21cc95851aa53479fe6dd305bca4ff87045fce
Jordan Booker <jordan.m.booker@gmail.com>
2020-11-13 15:54:03 -0500

    Styling for thanks page, test styling for link elements

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,15 +1,57 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import { Link } from 'gatsby'
+
+const thankYouContainer = {
+  marginTop: "7em",
+  marginBottom: "7em",
+}
+
+const thankYou = {
+  maxWidth: "844px",
+  margin: "auto",
+}
+
+const aboutTitle = {
+  color: "#BA5930",
+  fontSize: '3em',
+}
+
+const aboutText = {
+  fontSize: '1.65em',
+}
+
+const linkWrapper = {
+  backgroundColor: "#FAB395",
+  opacity: ".5",
+}
+
+const linkHighlight = {
+  textDecoration: "none",
+  color: "#2D2C2C",
+}
 
 export default () => (
   <Layout>
-    <section className="section">
-      <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
-        </div>
-      </div>
-    </section>
+<section className="section" style={thankYouContainer}>
+          <div className="container is-max-widescreen">
+            <div className="columns">
+              <div className="column" style={thankYou}>              
+              <h2 className="title" style={aboutTitle}>
+                Thanks for reaching out! 
+              </h2>
+              <p style={aboutText}>
+                We’ll be in touch within the next day or two. In the meantime,<br/>check out
+                {' '}
+                <span style={linkWrapper}>
+                  <Link to="/projects" style={linkHighlight}>
+                  this cool stuff.
+                  </Link>
+                </span>
+              </p>
+              </div>
+            </div>
+          </div>
+        </section>
   </Layout>
 )
