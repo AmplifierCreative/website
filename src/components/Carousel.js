e969be6c87ab2d70b299b284ec8e80ce41bf119d
Uonai <cs.mccaleb@gmail.com>
2020-12-12 12:16:23 -0600

    Custom field for featured projects, carousel integration

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,8 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
-import {Keyframes, delay, config} from 'react-spring/renderprops'
-
+import { Keyframes, delay, config } from 'react-spring/renderprops'
 
 import danielWhiteShow from '../img/clients/The_Daniel_White_Show_new.png'
 import freePizza from '../img/clients/FPP_podStack_color.png'
@@ -16,188 +15,225 @@ const logoImage = {
   zIndex: '1',
 }
 
-const Container =  Keyframes.Spring(async next =>
-  { while (true) {
-      await next({
-        from: { opacity: 0, left: '219px' },
-        left: '0px',
-        position: 'relative',
-        opacity: 1,
-        config: config.molasses 
-      })
-  }})
+const Container = Keyframes.Spring(async (next) => {
+  while (true) {
+    await next({
+      from: { opacity: 0, left: '219px' },
+      left: '0px',
+      position: 'relative',
+      opacity: 1,
+      config: config.molasses,
+    })
+  }
+})
 
-const CarouselItem1 =  Keyframes.Spring(async next =>
-  { while (true) {
-      await next({
-        from: { opacity: 1, left: '0px'},
-        left: '-229px',
-        position: 'relative',
-        opacity: 0,
-        delay: 3000,
-        config: config.slow
-      })
-      await next({
-        from: { opacity: 0, left: '916px'},
-        left: '687px',
-        position: 'relative',
-        opacity: 1,
-        config: config.molasses
-      })
-      await next({
-        from: {left: '687px'},
-        left: '458px',
-        position: 'relative',
-        config: config.molasses
-      })
-      await next({
-        from: {left: '458px'},
-        left: '229px',
-        position: 'relative',
-        config: config.molasses
-      })
-      await next({
-        from: {left: '229px'},
-        left: '0px',
-        position: 'relative',
-        config: config.molasses
-      })
-  }})
+const CarouselItem1 = Keyframes.Spring(async (next) => {
+  while (true) {
+    await next({
+      from: { opacity: 1, left: '0px' },
+      left: '-229px',
+      position: 'relative',
+      opacity: 0,
+      delay: 3000,
+      config: config.slow,
+    })
+    await next({
+      from: { opacity: 0, left: '916px' },
+      left: '687px',
+      position: 'relative',
+      opacity: 1,
+      config: config.molasses,
+    })
+    await next({
+      from: { left: '687px' },
+      left: '458px',
+      position: 'relative',
+      config: config.molasses,
+    })
+    await next({
+      from: { left: '458px' },
+      left: '229px',
+      position: 'relative',
+      config: config.molasses,
+    })
+    await next({
+      from: { left: '229px' },
+      left: '0px',
+      position: 'relative',
+      config: config.molasses,
+    })
+  }
+})
 
-  const CarouselItem2 =  Keyframes.Spring(async next =>
-    { while (true) {
-        await next({
-          from: { left: '0px'},
-          left: '-229px',
-          position: 'relative',
-          delay: 3000,
-          config: config.molasses 
-        })
-        await next({
-          from: { left: '-229px', opacity: 1,},
-          left: '-458px',
-          opacity: 0,
-          position: 'relative',
-          config: config.slow 
-        })
-    }})
+const CarouselItem2 = Keyframes.Spring(async (next) => {
+  while (true) {
+    await next({
+      from: { left: '0px' },
+      left: '-229px',
+      position: 'relative',
+      delay: 3000,
+      config: config.molasses,
+    })
+    await next({
+      from: { left: '-229px', opacity: 1 },
+      left: '-458px',
+      opacity: 0,
+      position: 'relative',
+      config: config.slow,
+    })
+  }
+})
 
-  const CarouselItem3 =  Keyframes.Spring(async next =>
-    { while (true) {
-      await next({
-        from: { left: '0px'},
-        left: '-229px',
-        position: 'relative',
-        delay: 3000,
-        config: config.molasses 
-      })
-      await next({
-        from: { left: '-229px'},
-        left: '-458px',
-        position: 'relative',
-        config: config.molasses 
-      })
-      await next({
-        from: { left: '-458px', opacity: 1,},
-        left: '-687px',
-        opacity: 0,
-        position: 'relative',
-        config: config.slow
-      })
-    }})
+const CarouselItem3 = Keyframes.Spring(async (next) => {
+  while (true) {
+    await next({
+      from: { left: '0px' },
+      left: '-229px',
+      position: 'relative',
+      delay: 3000,
+      config: config.molasses,
+    })
+    await next({
+      from: { left: '-229px' },
+      left: '-458px',
+      position: 'relative',
+      config: config.molasses,
+    })
+    await next({
+      from: { left: '-458px', opacity: 1 },
+      left: '-687px',
+      opacity: 0,
+      position: 'relative',
+      config: config.slow,
+    })
+  }
+})
 
-    const CarouselItem4 =  Keyframes.Spring(async next =>
-      { while (true) {
-        await next({
-          from: { left: '0px'},
-          left: '-229px',
-          position: 'relative',
-          delay: 3000,
-          config: config.molasses 
-        })
-        await next({
-          from: { left: '-229px'},
-          left: '-458px',
-          position: 'relative',
-          config: config.molasses 
-        })
-        await next({
-          from: { left: '-458px',},
-          left: '-687px',
-          position: 'relative',
-          config: config.molasses 
-        })
-        await next({
-          from: { left: '-687px', opacity: 1,},
-          left: '-916px',
-          opacity: 0,
-          position: 'relative',
-          config: config.slow,
-        })
-      }})
-
+const CarouselItem4 = Keyframes.Spring(async (next) => {
+  while (true) {
+    await next({
+      from: { left: '0px' },
+      left: '-229px',
+      position: 'relative',
+      delay: 3000,
+      config: config.molasses,
+    })
+    await next({
+      from: { left: '-229px' },
+      left: '-458px',
+      position: 'relative',
+      config: config.molasses,
+    })
+    await next({
+      from: { left: '-458px' },
+      left: '-687px',
+      position: 'relative',
+      config: config.molasses,
+    })
+    await next({
+      from: { left: '-687px', opacity: 1 },
+      left: '-916px',
+      opacity: 0,
+      position: 'relative',
+      config: config.slow,
+    })
+  }
+})
 
 class Carousel extends React.Component {
-
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <Container>{ styles =>
-      <div className="carousel" style={styles}>
-            {/* posts &&
-            posts.map(({ node: post }) => (
+      <Container>
+        {(styles) => (
+          <div className="carousel" style={styles}>
+            {posts &&
+              posts.map(({ node: post }) => (
                 <div className="column" key={post.id}>
-                    <Link to={post.fields.slug}>
-                        <div>
-                            {post.frontmatter.featuredimage ? (
-                            <div className="featured-thumbnail">
-                                <PreviewCompatibleImage
-                                imageInfo={{
-                                    image: post.frontmatter.featuredimage,
-                                    alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                                }}
-                                />
-                            </div>
-                            ) : null}
+                  <Link to={post.fields.slug}>
+                    <div>
+                      {post.frontmatter.featuredimage ? (
+                        <div className="featured-thumbnail">
+                          <PreviewCompatibleImage
+                            imageInfo={{
+                              image: post.frontmatter.featuredimage,
+                              alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                            }}
+                          />
                         </div>
-                    </Link>
+                      ) : null}
+                    </div>
+                  </Link>
                 </div>
-            )) */}
-            <CarouselItem1>{styles => 
-              <div className="carousel-item-container is-active" style={styles}>
-                <a title="Free Pizza Podcast" href="#">
-                  <img src={freePizza} alt="Free Pizza Podcast" style={logoImage}/>
-                </a>
-              </div>}
+              ))}
+            <CarouselItem1>
+              {(styles) => (
+                <div
+                  className="carousel-item-container is-active"
+                  style={styles}
+                >
+                  <a title="Free Pizza Podcast" href="#">
+                    <img
+                      src={freePizza}
+                      alt="Free Pizza Podcast"
+                      style={logoImage}
+                    />
+                  </a>
+                </div>
+              )}
             </CarouselItem1>
-            <CarouselItem2>{styles => 
-              <div className="carousel-item-container is-active" style={styles}>
-                <a title="Daniel White Show" href="#">
-                  <img src={danielWhiteShow} alt="Daniel White Show" style={logoImage}/>
-                </a>
-              </div>}
+            <CarouselItem2>
+              {(styles) => (
+                <div
+                  className="carousel-item-container is-active"
+                  style={styles}
+                >
+                  <a title="Daniel White Show" href="#">
+                    <img
+                      src={danielWhiteShow}
+                      alt="Daniel White Show"
+                      style={logoImage}
+                    />
+                  </a>
+                </div>
+              )}
             </CarouselItem2>
-            <CarouselItem3>{styles => 
-              <div className="carousel-item-container is-active" style={styles}>
-                <a title="Audpop" href="#">
-                    <img src={audpop} alt="Audpop" style={logoImage}/>
-                </a>
-              </div>}
+            <CarouselItem3>
+              {(styles) => (
+                <div
+                  className="carousel-item-container is-active"
+                  style={styles}
+                >
+                  <a title="Audpop" href="#">
+                    <img src={audpop} alt="Audpop" style={logoImage} />
+                  </a>
+                </div>
+              )}
             </CarouselItem3>
-            <CarouselItem4>{styles => 
-            <div className="carousel-item-container is-active" style={styles}>
-              <a title="Caridnal Joinery" href="#">
-                  <img src={cardinal} alt="Cardinal Joinery Logo" style={logoImage} />
-              </a>
-            </div>}
+            <CarouselItem4>
+              {(styles) => (
+                <div
+                  className="carousel-item-container is-active"
+                  style={styles}
+                >
+                  <a title="Caridnal Joinery" href="#">
+                    <img
+                      src={cardinal}
+                      alt="Cardinal Joinery Logo"
+                      style={logoImage}
+                    />
+                  </a>
+                </div>
+              )}
             </CarouselItem4>
-        </div>}
-        </Container>   
+          </div>
+        )}
+      </Container>
     )
   }
-} 
+}
 
 Carousel.propTypes = {
   data: PropTypes.shape({
@@ -213,7 +249,12 @@ export default () => (
       query CarouselQuery {
         allMarkdownRemark(
           sort: { order: DESC, fields: [frontmatter___date] }
-          filter: { frontmatter: { templateKey: { eq: "project-post" } } }
+          filter: {
+            frontmatter: {
+              templateKey: { eq: "project-post" }
+              featuredpost: { eq: true }
+            }
+          }
         ) {
           edges {
             node {
@@ -227,7 +268,8 @@ export default () => (
                 templateKey
                 date(formatString: "MMMM DD, YYYY")
                 tags
-                featuredimage {
+                featuredpost
+                clientlogo {
                   childImageSharp {
                     fluid(maxWidth: 650, quality: 100) {
                       ...GatsbyImageSharpFluid
--- src/pages/projects/2020-11-10-project-title.md
+++ src/pages/projects/2020-11-10-project-title.md
@@ -3,6 +3,8 @@ templateKey: project-post
 title: Project Title
 date: 2020-11-10T02:55:34.791Z
 description: Test
+featuredpost: true
+clientlogo: /img/the_daniel_white_show_new.png
 featuredimage: /img/placeholder-2.jpg
 tags:
   - Copy
